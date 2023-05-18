b4b97faa921454781f7707bd07eb1312aa2b3969
Anik Barua <anikbb04@gmail.com>
2023-05-18 21:15:57 +0600

    make require auth for unauthorized user

---
+++ src/Components/Shared/Authentication/RequireAuth/RequireAuth.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { LegoContext } from '../../../AuthProvider/AuthProvider';
+import { Navigate, useLocation } from 'react-router-dom';
+
+const RequireAuth = ({ children }) => {
+
+    const { user, loading } = useContext(LegoContext)
+    const location = useLocation();
+
+    if (loading) {
+        return <LoadingSpinner></LoadingSpinner>
+    }
+    if (!user) {
+        return <Navigate to={'/login'} state={{ from: location }} replace></Navigate>
+    }
+    return children;
+};
+
+export default RequireAuth;
